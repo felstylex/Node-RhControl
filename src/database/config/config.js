813de28d52f20658ca939a444291d01ae7b578ab
felipe <felipestylex08@gmail.com>
2023-07-11 10:29:41 -0300

    config

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,23 +1,14 @@
-{
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "database_development",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "test": {
-    "username": "root",
-    "password": null,
-    "database": "database_test",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  },
-  "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
-  }
-}
+const Sequelize = require('sequelize');
+const path = require('path');
+require('dotenv').config({path: path.join(__dirname, '../','.env')});
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    dialect: 'mysql',
+    host: process.env.DB_HOST,
+    port: process.env.PORT,
+}, {
+    define: {
+       freezeTableName: true, 
+    }
+});
+module.exports = sequelize;
